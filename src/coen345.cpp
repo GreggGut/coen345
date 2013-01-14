aa1f6191c7504b7392d741ca2d0545bd3294743e
GreggGut <gregg.gut@gmail.com>
2013-01-14 15:56:56 -0500

    Testing

---
+++ src/coen345.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : coen345.cpp
+// Author      : Grzegorz Gut
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
